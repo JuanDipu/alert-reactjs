dd47b85f33e5768e62b43179d19ad2000a942ad5
Juan Diego <u1802847@unimilitar.edu.co>
2021-07-13 16:36:08 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import swal from "sweetalert";
 
 function App() {
+  const mostrarAlerta = () => {
+    swal({
+      title: "Hola !!!!",
+      text: "Ejercicio alerta y React.js",
+      icon: "info",
+      button: "Aceptar",
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="boton" onClick={() => mostrarAlerta()}>
+          Mostrar Alerta
+        </button>
       </header>
     </div>
   );
